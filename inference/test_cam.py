54e7eb3fbe1483f5ad6908401c24a38b863065e9
Trần Vương Quốc Đạt <Tranvuongquocdat@gmail.com>
2025-03-26 11:54:30 +0700

    abc

--- inference/test_cam.py
+++ inference/test_cam.py
@@ -1,25 +1,24 @@
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 import time
 
 # Khởi tạo đối tượng Picamera2
 picam2 = Picamera2()
 
-# Tạo cấu hình xem trước mặc định
+# Tạo cấu hình mặc định
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 
-# Bật chế độ xem trước (preview)
-picam2.start_preview(Preview.QTGL)  # Sử dụng QTGL để hiển thị cửa sổ xem trước
+# Bật camera
+print("Đang khởi động camera...")
 picam2.start()
 
-# Chờ 5 giây để xem trước
-print("Đang hiển thị preview, kiểm tra màn hình...")
+# Chờ 5 giây
 time.sleep(5)
 
-# Chụp một bức ảnh và lưu dưới tên "test_image.jpg"
+# Chụp ảnh và lưu
+print("Đang chụp ảnh...")
 picam2.capture_file("test_image.jpg")
-print("Đã chụp ảnh, kiểm tra file 'test_image.jpg' trong thư mục hiện tại.")
+print("Đã chụp ảnh, kiểm tra file 'test_image.jpg'.")
 
 # Dừng camera
-picam2.stop_preview()
 picam2.stop()
